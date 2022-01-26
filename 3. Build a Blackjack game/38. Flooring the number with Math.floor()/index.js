47f9cd69fc6a2f379703a99f9a8603a36075b924
Karla <karlavdelgadof@gmail.com>
2022-01-26 14:45:14 -0500

    Add answer

--- 3. Build a Blackjack game/38. Flooring the number with Math.floor()/index.js
+++ 3. Build a Blackjack game/38. Flooring the number with Math.floor()/index.js
@@ -1,6 +1,6 @@
 // let randomNumber = Math.random() * 6
 
-let flooredNumber = Math.floor(3.45632)
+let flooredNumber = Math.floor(5.45632)
 
 console.log(flooredNumber)
 
@@ -9,6 +9,5 @@ console.log(flooredNumber)
 
 What does Math.floor() do to positive numbers?
 
-Your answer: 
-
+Your answer: It removes decimals
 */
