2f8c0b78305e532fc2186502676f0eb499a5c230
Karla <karlavdelgadof@gmail.com>
2022-01-26 16:45:44 -0500

    Create object and log out keys

--- 3. Build a Blackjack game/51. Create your first object/index.js
+++ 3. Build a Blackjack game/51. Create your first object/index.js
@@ -2,4 +2,17 @@
 // It should contain at least one boolean, one string, one number, and one array
 // Log out at least two of the keys using the dot notation
 
+let airbnbCastleList = {
+    location: "Tuscana",
+    name: "San Giusto Abbey",
+    description: [
+        "4 guests",
+        "2 bedrooms",
+        "2 baths"
+    ],
+    price: 190,
+    avaliable: true
+}
 
+console.log( airbnbCastleList.description)
+console.log(airbnbCastleList.price)
