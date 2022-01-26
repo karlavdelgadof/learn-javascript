687df02f6b2b47d0378be8552df02d2f64447e04
Karla <karlavdelgadof@gmail.com>
2022-01-25 19:38:37 -0500

    Create an array

--- 3. Build a Blackjack game/21. Intro to arrays/index.js
+++ 3. Build a Blackjack game/21. Intro to arrays/index.js
@@ -9,3 +9,8 @@ let featuredPosts = [
 // Create an array that lists your i.e. experience, education, licenses, skills or similar
 // The items of the array should be strings
 
+let educationRecord = [
+    "Microverse certificated student",
+    "Graduated summa cum laude from University of Alberta",
+    "Computational Science and Engineering SM Master"
+]
