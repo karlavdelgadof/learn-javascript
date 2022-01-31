bef7d4fdcfe84ba57394d9936cee221b64c1127e
Karla <karlavdelgadof@gmail.com>
2022-01-31 15:54:29 -0500

    Add function

--- 4. Practice time - part 2/6. Rock papers scissors/index.js
+++ 4. Practice time - part 2/6. Rock papers scissors/index.js
@@ -2,4 +2,8 @@ let hands = ["rock", "paper", "scissor"]
 
 // Create a function that returns a random item from the array
 
-
+function getRandomItem() {
+    let randomIndex = Math.floor( Math.random() * 3 )
+    return hands [randomIndex]
+}
+ console.log( getRandomItem() )
