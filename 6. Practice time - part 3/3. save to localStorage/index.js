f2e94fd56a1e73115af7e4532f5de3c7dc599d9f
Karla <karlavdelgadof@gmail.com>
2022-02-07 13:43:11 -0500

    Save/fetch value from localStorage

--- 6. Practice time - part 3/3. save to localStorage/index.js
+++ 6. Practice time - part 3/3. save to localStorage/index.js
@@ -1,3 +1,7 @@
 // Save a value to localStorage
 // Delete your code and refresh the page
 // Fetch your value from localStorage and log it out
+
+const storedValue = localStorage.getItem("myCrush")
+
+console.log(storedValue)
