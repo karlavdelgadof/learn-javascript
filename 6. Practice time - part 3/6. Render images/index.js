72c43b0fb8dde5b1e736c348dc64af4ecb2e4153
Karla <karlavdelgadof@gmail.com>
2022-02-07 14:51:04 -0500

    Create function to render images properly

--- 6. Practice time - part 3/6. Render images/index.js
+++ 6. Practice time - part 3/6. Render images/index.js
@@ -7,3 +7,16 @@ const imgs = [
     "images/hip2.jpg",
     "images/hip3.jpg"
 ]
+
+const container = document.getElementById("container")
+
+function renderImages() {
+    let imgsDOM = ""
+    for (let i = 0; i < imgs.length; i++ ) {
+        imgsDOM += `<img alt='Employee in the company' class='team-img' src='${imgs[i]}'>`
+    }
+
+    container.innerHTML = imgsDOM
+}
+
+renderImages()
